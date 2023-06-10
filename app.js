9a1b99a6b073b23b05b6c1d1149e44df895f160c
thenam176Xz <thenam081279@gmail.com>
2023-06-09 22:26:08 -0400

    first Init

--- app.js
+++ app.js
@@ -51,37 +51,3 @@ app.set('view engine', 'ejs')
 // Listen
 app.listen(port, () => console.info(`Linstening on port ${port}`))
 
-/*
-// import
-const express = require('express')
-const app = express()
-const port = 3000
-
-// Static Files
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/img', express.static(__dirname + 'public/img'))
-
-// Set views
-app.set('views', './views')
-app.set('view engine', 'ejs')
-
-app.get('', (req, res) =>{
-    res.render(__dirname +'/views/index.ejs')
-})
-app.get('/aboutMe', (req, res) =>{
-    res.render(__dirname +'/views/aboutMe.ejs')
-})
-app.get('/contact', (req, res) =>{
-    res.render(__dirname +'/views/contact.ejs')
-})
-app.get('/projectPage', (req, res) =>{
-    res.render(__dirname +'/views/projectPage.ejs')
-})
-app.get('/servicePage', (req, res) =>{
-    res.render(__dirname +'/views/servicePage.ejs')
-})
-// Listen on port 3000
-app.listen(port, () => console.info(`Linstening on port ${port}`))
-*/
